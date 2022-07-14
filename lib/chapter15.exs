c6cf65b976cb179d2d6d0e0ff48a6753176a8b1a
YanethM <yaneth.mejiar@autonoma.edu.co>
2022-07-14 16:05:06 -0500

    Chapters 13, 14 y 15

--- lib/chapter15.exs
+++ lib/chapter15.exs
@@ -1,3 +1,32 @@
 defmodule User do
-  defstruct name: "John", age: 27
+  @doc """
+  commands:
+  iex> %User{}
+  iex> %User{name: "Yaneth"}
+  iex> yaneth.age
+  iex> jhon = %{yaneth | name: "Jhon"}
+  iex> is_map(yaneth)
+  iex> yaneth.__struct__
+  iex> yaneth = %User{}
+  iex> yaneth = Map.put(%User{}, :name, "Yaneth")
+  iex> yaneth = Map.merge(yaneth,%User{name: Jhon})
+  iex> Map.keys(yaneth)
+  """
+  defstruct name: "Yaneth", age: 31
+end
+
+defmodule UserNil do
+  @doc """
+  commands:
+  iex> %UserNil{}
+  """
+  defstruct [:email, name: "Yaneth", age: 31]
+end
+
+defmodule Car do
+  @doc """
+  iex> Car.__struct__
+  """
+  @enforce_keys [:make]
+  defstruct [:model, make: "Mazda"]
 end
